3d06cf59a54c554aa2576cf883fbef057fffa572
360Parminder <yesask8@gmail.com>
2025-06-02 18:49:37 +0530

    feat: add new project images and GitHub links to Projects component

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,35 +1,52 @@
 import React from 'react';
 import { motion } from 'framer-motion';
+import rajdoot from '../assets/images/rajdoot.png';
+import aura from '../assets/images/aura.png';
+import slug from '../assets/images/slug.png';
+import swasthya from '../assets/images/swasthya.png';
 
 const projects = [
   {
     title: "Rajdoot",
     description: "Sophisticated Messaging APIs for Developers. Build powerful messaging applications with our elegant, reliable, and affordable API platform.",
-    image: "https://images.pexels.com/photos/3183150/pexels-photo-3183150.jpeg",
-    tech: ["React", "Node.js", "MongoDB","IOT","Razorpay"],
-    link: "https://rajdoot.parminder.info"
+    image: rajdoot,
+    tech: ["React", "Node.js", "MongoDB","IOT","Razorpay","Express","AWS","Cloudinary","IOT","Arduino","MQTT","Socket.io"],
+    link: "https://rajdoot.parminder.info",
+    github: "https://github.com/360parminder/rajdoot"
+  },
+  {
+    title: "Swasthya",
+    description: "Swasthya is a platform that allows you to monitor your health and get alerts when your health is not good. It also allow you to track your family and friends medication and health.",
+    image: swasthya,
+    tech: ["React Native", "Node.js", "Tailwind","MongoDB","Express","AWS","Cloudinary","IOT","React"],
+    link: "https://swasthya.parminder.info/",
+    github: "https://github.com/360parminder/swasthya"
   },
   {
     title: "Aura",
     description: "Mobile-first responsive design with modern animations",
-    image: "https://images.pexels.com/photos/3183153/pexels-photo-3183153.jpeg",
+    image: aura,
     tech: ["React", "Firebase", "Tailwind","Express","Node.js","MongoDB","Razorpay"],
-    link: "https://auravideos.vercel.app"
+    link: "https://auravideos.vercel.app",
+    github: "https://github.com/360Parminder/Aura-Frontend"
+
   },
   {
     title: "The Slug ",
     description: "URl Shortener with a twist. Shorten your URLs and share them with the world. The Slug is a URL shortener that allows you to create short links for your long URLs.",
-    image: "https://images.pexels.com/photos/3183132/pexels-photo-3183132.jpeg",
+    image: slug,
     tech: ["React", "Node.js", "Tailwind","MongoDB","Express",],
-    link: "slug-url.vercel.app"
+    link: "slug-url.vercel.app",
+    github: "https://github.com/360Parminder/TheSlug"
   },
-  {
-    title: "Quick Cart",
-    description: "Bill your customers in seconds. Quick Cart is a simple and easy to use cart application that allows you to create and manage your cart.",
-    image: "https://images.pexels.com/photos/3183132/pexels-photo-3183132.jpeg",
-    tech: ["React", "Node.js", "Tailwind","MongoDB","Express","Razorpay"],
-    link: "https://quickcart-store.vercel.app"
-  }
+  // {
+  //   title: "Quick Cart",
+  //   description: "Bill your customers in seconds. Quick Cart is a simple and easy to use cart application that allows you to create and manage your cart.",
+  //   image: "https://images.pexels.com/photos/3183132/pexels-photo-3183132.jpeg",
+  //   tech: ["React", "Node.js", "Tailwind","MongoDB","Express","Razorpay"],
+  //   link: "https://quickcart-store.vercel.app"
+  // }
+  
 ];
 
 const Projects = () => {
@@ -72,9 +89,17 @@ const Projects = () => {
                 </div>
                 <a
                   href={project.link}
+                  target="_blank"
                   className="inline-block px-6 py-2 bg-gradient-to-r from-blue-500 to-purple-500 rounded-full text-white font-medium hover:opacity-90 transition-opacity"
                 >
                   View Project
+                </a>
+                <a
+                  href={project.github}
+                  target="_blank"
+                  className="inline-block ml-2 px-6 py-2 bg-gradient-to-r from-blue-500 to-purple-500 rounded-full text-white font-medium hover:opacity-90 transition-opacity"
+                >
+                  Github
                 </a>
               </div>
             </motion.div>
